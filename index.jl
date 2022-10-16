32eae5710e5334a5b963006b2f95ba5c53dcaeb2
ppmzhang2 <ppmzhang2@users.noreply.github.com>
2022-10-16 12:27:57 +0000

    deploy: bc523cecc784868dc8f3ae92603810c1937a3b96

---
+++ index.jl
@@ -0,0 +1,44 @@
+### A Pluto.jl notebook ###
+# v0.19.13
+
+#> [frontmatter]
+#> title = "Pluto Notebooks"
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 5c6b4d06-4c09-11ed-0344-45fa15561fca
+md"""
+# Pluto Notebooks
+
+## Table of Content
+
+- ODE
+  - [Simple Harmonic Motion](./ode_harmonic.html)
+  - [Orbit of the Moon](./ode_orbit.html)
+  - [Solving Single Pendulum with Lagrangian Mechanics](./ode_pendulum.html)
+  - [Brachistochrone Problem](./ode_brachistochrone.html)
+- CV
+  - [IOU vs. GIOU](./cv_iou.html)
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.2"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─5c6b4d06-4c09-11ed-0344-45fa15561fca
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
